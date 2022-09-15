bfa0f53047b9e28316e4e7bc149859260cf47cac
Haifei Zhang <haifei.zhang@ti8m.ch>
2022-09-15 12:43:00 +0200

    add types

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export interface IList {
+  city: string;
+  floors: number;
+  id: string;
+  image: string;
+  name: string;
+  price: number;
+  rooms: number;
+  sqft: number;
+  state: string;
+}
+
+export interface IData {
+  listings: IList[];
+}
